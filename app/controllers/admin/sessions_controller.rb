c4fdb94038801a80766ed6e041ab34cd47677b1a
suzukinanae55018 <165752907+suzukinanae55018@users.noreply.github.com>
2024-07-06 07:48:33 +0000

    [Fix] 管理者ログイン遷移先修正

--- app/controllers/admin/sessions_controller.rb
+++ app/controllers/admin/sessions_controller.rb
@@ -5,7 +5,7 @@ class Admin::SessionsController < Devise::SessionsController
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   def after_sign_in_path_for(resource)
-    admin_users_path
+    admin_path
   end
 
   def after_sign_out_path_for(resource)
