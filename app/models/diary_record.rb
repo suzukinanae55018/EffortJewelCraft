6dd1519e3b65fafb76849793cb6fc12ac70012df
suzukinanae55018 <165752907+suzukinanae55018@users.noreply.github.com>
2024-07-09 05:28:56 +0000

    [Add] ページネーションの記述追加

--- app/models/diary_record.rb
+++ app/models/diary_record.rb
@@ -1,7 +1,7 @@
 class DiaryRecord < ApplicationRecord
 
-  validates :title, presence: true, length: { maximum: 20 }
-  validates :body, presence: true, length:  { maximum: 300 }
+  validates :title, presence: true, length: { maximum: 30 }
+  validates :body, presence: true, length:  { maximum: 200 }
   validates :category, presence: true,length: { maximum: 50 }
 
   has_one_attached :diary_record_image
@@ -15,8 +15,8 @@ class DiaryRecord < ApplicationRecord
   
   def get_diary_record_image(width, height)
     unless diary_record_image.attached?
-      file_path = Rails.root.join("app/assets/images/diamonds.jpg")
-      diary_record_image.attach(io: File.open(file_path), filename: "diamonds.jpg", content_type: "image/jpeg")
+      file_path = Rails.root.join("app/assets/images/touka.png")
+      diary_record_image.attach(io: File.open(file_path), filename: "touka.png", content_type: "image/png")
     end
     diary_record_image.variant(resize_to_limit: [width, height]).processed
   end
@@ -24,8 +24,8 @@ class DiaryRecord < ApplicationRecord
   # 後で背景を選べるようにする
   def get_background_image(width, height)
     unless background_image.attached?
-      file_path = Rails.root.join("app/assets/images/blue.jpg")
-      background_image.attach(io: File.open(file_path), filename: "blue.jpg", content_type: "image/jpeg")
+      file_path = Rails.root.join("app/assets/images/touka.png")
+      background_image.attach(io: File.open(file_path), filename: "touka.png", content_type: "touka.png")
     end
     background_image.variant(resize_to_limit: [width, height]).processed
   end
