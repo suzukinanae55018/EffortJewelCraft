938e3d1b0c921914d56fc5373368e688f2861d76
suzukinanae55018 <165752907+suzukinanae55018@users.noreply.github.com>
2024-07-12 15:00:27 +0000

    [Add] 管理者側投稿削除機能実装

--- app/controllers/admin/diary_records_controller.rb
+++ app/controllers/admin/diary_records_controller.rb
@@ -1,7 +1,20 @@
 class Admin::DiaryRecordsController < ApplicationController
+  before_action :authenticate_admin!
+
   def index
+    @diary_records = DiaryRecord.all.order(created_at: :desc).page(params[:page]).per(15)
   end
 
+
   def destroy
+    diary_record = DiaryRecord.find(params[:id])
+
+    if diary_record.destroy
+      flash[:notice] = "投稿の削除に成功しました"
+      redirect_to request.referer
+    else
+      flash.now[:alert] = "投稿の削除に失敗しました。"
+      render :edit
+    end
   end
 end
