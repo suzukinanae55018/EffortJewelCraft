b15421d999cae6e87cf7e2b489fd4596f769c94f
suzukinanae55018 <165752907+suzukinanae55018@users.noreply.github.com>
2024-07-12 09:42:04 +0000

    [Add] サーチ機能ビューの記述追加

--- app/controllers/public/searches_controller.rb
+++ app/controllers/public/searches_controller.rb
@@ -5,9 +5,9 @@ before_action :authenticate_user!
     @range = params[:range]
 
     if @range == "User"
-      @users = User.looks(params[:search], params[:word])
+      @users = User.looks(params[:search], params[:word]).page(params[:page]).per(15)
     else
-      @diary_records = DiaryRecord.looks(params[:search], params[:word])
+      @diary_records = DiaryRecord.looks(params[:search], params[:word]).page(params[:page]).per(15)
     end
   end
 end
