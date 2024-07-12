54fcad0ecfee668f85ce4492b3d67db89a336973
suzukinanae55018 <165752907+suzukinanae55018@users.noreply.github.com>
2024-07-12 09:00:47 +0000

    [Add] サーチ機能追加

--- app/controllers/public/searches_controller.rb
+++ app/controllers/public/searches_controller.rb
@@ -1,6 +1,13 @@
 class Public::SearchesController < ApplicationController
-  
+before_action :authenticate_user!
+
   def search
-    
+    @range = params[:range]
+
+    if @range == "User"
+      @users = User.looks(params[:search], params[:word])
+    else
+      @diary_records = DiaryRecord.looks(params[:search], params[:word])
+    end
   end
 end
