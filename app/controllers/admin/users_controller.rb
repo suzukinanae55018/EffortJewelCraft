7142e70f5e43d471b091577968984f2e93a46e2c
suzukinanae55018 <165752907+suzukinanae55018@users.noreply.github.com>
2024-07-12 14:49:40 +0000

    [Add] 管理者側ユーザー削除機能追加

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,16 +1,34 @@
 class Admin::UsersController < ApplicationController
+  before_action :authenticate_admin!
+
   def index
+    @users = User.all.order(created_at: :desc).page(params[:page]).per(15)
   end
 
   def show
+    @user = User.find(params[:id])
+    @diary_records = @user.diary_records.order(created_at: :desc).page(params[:page]).per(15)
   end
 
   def edit
   end
 
   def destroy
+      user = User.find(params[:id])
+      if user.destroy
+        flash[:notice] = "ユーザーを削除しました。"
+        redirect_to admin_users_path
+      else
+        flash.now[:alert] = "ユーザーの削除に失敗しました。"
+        render :show
+      end
   end
 
   def update
   end
+
+  private
+    def user_params
+      params.require(:user).permit(:name, :introduction, :profile_image)
+    end
 end
