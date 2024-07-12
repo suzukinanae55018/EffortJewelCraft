5ae7df042f3c5665222fe32a733b6b8227de4ef4
suzukinanae55018 <165752907+suzukinanae55018@users.noreply.github.com>
2024-07-12 06:21:21 +0000

    [Add] searchescontroller追加

--- config/routes.rb
+++ config/routes.rb
@@ -45,6 +45,7 @@ Rails.application.routes.draw do
     devise_scope :user do
       post "users/guest_sign_in", to: "sessions#guest_sign_in"
     end
+    get "search" => "searches#search"
     # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   end
 end
