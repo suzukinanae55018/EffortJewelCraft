69d02c5f0b6863c93576ef4cf6a26c2ef19136ce
suzukinanae55018 <165752907+suzukinanae55018@users.noreply.github.com>
2024-07-20 03:44:53 +0000

    [Fix,Add] グループ詳細ページレイアウト修正、追記

--- app/controllers/public/groups_controller.rb
+++ app/controllers/public/groups_controller.rb
@@ -30,6 +30,8 @@ class Public::GroupsController < ApplicationController
 
   def show
     @group = Group.find(params[:id])
+    @user = User.find(params[:id])
+    @diary_records = @group.users.joins(:diary_records).select('diary_records.*').page(params[:page]).per(10)
   end
 
   def edit
