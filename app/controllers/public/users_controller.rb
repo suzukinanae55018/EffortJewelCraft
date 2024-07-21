47099e5fba1cd98171599c137316a873f250f81d
suzukinanae55018 <165752907+suzukinanae55018@users.noreply.github.com>
2024-07-21 05:50:24 +0000

    [Fix] Eメール情報変更

--- app/controllers/public/users_controller.rb
+++ app/controllers/public/users_controller.rb
@@ -48,7 +48,7 @@ class Public::UsersController < ApplicationController
 
   private
     def user_params
-      params.require(:user).permit(:name, :introduction, :profile_image)
+      params.require(:user).permit(:name, :introduction, :profile_image, :email)
     end
 
     def is_matching_login_user
