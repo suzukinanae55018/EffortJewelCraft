fc0243e82ee2de1ca5c20191c5a7eb05325ed75c
suzukinanae55018 <165752907+suzukinanae55018@users.noreply.github.com>
2024-07-26 07:42:29 +0000

    [Fix] 検索窓を日本語に変更

--- app/controllers/public/searches_controller.rb
+++ app/controllers/public/searches_controller.rb
@@ -4,7 +4,7 @@ class Public::SearchesController < ApplicationController
   def search
     @range = params[:range]
 
-    if @range == "User"
+    if @range == "ユーザー"
       @users = User.looks(params[:search], params[:word]).page(params[:page]).per(14)
     else
       @diary_records = DiaryRecord.looks(params[:search], params[:word]).order(created_at: :desc).page(params[:page]).per(14)
